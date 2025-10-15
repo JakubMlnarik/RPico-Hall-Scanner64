0a940bf73efb935fa82028e0f47bc4556ad7fb2b
JMlnarikRockwell <jakub.mlnarik@rockwellautomation.com>
2025-10-15 19:14:04 +0200

    WIP

--- src/midi.c
+++ src/midi.c
@@ -37,21 +37,41 @@ void midi_process(SETTINGS *set, critical_section_t *cs, queue_t *buff) {
     static uint16_t prev[HALL_SCANNER_TOTAL_CHANNELS] = {0};
     uint16_t curr[HALL_SCANNER_TOTAL_CHANNELS] = {0};
     static bool on_state[HALL_SCANNER_TOTAL_CHANNELS] = {false};
+
+    // FIFO buffer for last 15 values of channel 0
+    #define FIFO_SIZE 15
+    static uint16_t fifo[FIFO_SIZE] = {0};
+    static int fifo_index = 0;
+    static int fifo_count = 0;
+
     while (true) {
         hall_scanner_read_all(curr);
+
+        // Update FIFO for channel 0
+        fifo[fifo_index] = curr[0];
+        fifo_index = (fifo_index + 1) % FIFO_SIZE;
+        if (fifo_count < FIFO_SIZE) fifo_count++;
+
         for (int i = 0; i < HALL_SCANNER_TOTAL_CHANNELS; ++i) {
             if (i < MIDI_NO_TONES) { // I am interested only about the real physical sensors
                 if (curr[i] > set->on_voltage_threshold[i] && prev[i] <= set->on_voltage_threshold[i] && on_state[i] == false) {
                     printf("NOTE ON: %d\n", i);
                     midi_send_note_on(set->m_ch, set->m_base, i, 127, cs, buff);
                     on_state[i] = true;
+
+                    // Print FIFO buffer for channel 0 only when note on is detected
+                    printf("Last %d voltages for channel 0: ", fifo_count);
+                    for (int j = 0; j < fifo_count; ++j) {
+                        int idx = (fifo_index + j) % FIFO_SIZE;
+                        printf("%u ", fifo[idx]);
+                    }
+                    printf("\n");
                 } else if (curr[i] <= set->off_voltage_threshold[i] && prev[i] > set->off_voltage_threshold[i] && on_state[i] == true) {
                     printf("NOTE OFF: %d\n", i);
                     midi_send_note_off(set->m_ch, set->m_base, i, cs, buff);
                     on_state[i] = false;
                 }
             }
-            
             prev[i] = curr[i];
         }
     }
